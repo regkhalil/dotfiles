a0c7d989323e232263618e1dda2d5e4e9a972b3c
regkhalil <khalil.regaieg@outlook.com>
2025-05-05 15:15:35 +0100

    Added wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+
+
+
+
+
+-- set up the colour scheme
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Gruvbox Dark (Gogh)'
+  else
+    return 'Gruvbox (Gogh)'
+  end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
+
+
+-- set the font
+config.font = wezterm.font 'Fira Code'
+
+
+
+return config
