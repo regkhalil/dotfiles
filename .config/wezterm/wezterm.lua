ed63f407b1dcae8155c13a5bed036f2b33e9143c
regkhalil <khalil.regaieg@outlook.com>
2025-05-06 13:12:13 +0100

    wezterm hide title bar

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -30,7 +30,7 @@ config.color_scheme = scheme_for_appearance(get_appearance())
 -- set the font
 config.font = wezterm.font 'Fira Code Medium'
 
-
+config.window_decorations = "NONE"
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_wayland = false
 
